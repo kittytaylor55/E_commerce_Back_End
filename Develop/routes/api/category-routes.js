fa9e70dd9576d7956a31baf9c130b5febfca23ca
Crystal Taylor <kittytaylor@gmail.com>
2021-06-09 23:29:00 -0700

    Editing routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,9 +3,14 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    cinst category = await Category.findAll({
+      include: [{model: Product, as: "products"}]
+    })
+  }
 });
 
 router.get('/:id', (req, res) => {
